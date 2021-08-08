9820f4d0c6207586752dd31d60e7238a6cc29b4e
duyng2512 <duyng2512@gmail.com>
2021-08-08 23:07:29 +0700

    v2

---
+++ util/v2/util.go
@@ -0,0 +1,16 @@
+package util 
+
+import (
+	"fmt"
+)
+
+func Util(){
+	fmt.Println("[Util] pck")
+	fmt.Println("[Ver] 2.0.1")
+}
+
+func Info(){
+	fmt.Println("[Util] pck")
+	fmt.Println("[Ver] 2.0.1")
+}
+
